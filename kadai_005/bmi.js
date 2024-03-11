144532fc6d2eb9c24711f69b452c04858152bc21
Arrietty1219 <158137190+Arrietty1219@users.noreply.github.com>
2024-03-11 23:43:31 +0900

    Update bmi.js

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,6 +1,12 @@
+// 変数の宣言
+let bmi;
+
 // 定数の宣言・値の代入
 const weight = 68;
 const height = 1.7;
 
-//BMI算出
-console.log(weight / height / height);
+// 値の代入
+bmi = weight / (height * height);
+
+// コンソールへの出力
+console.log(bmi);
